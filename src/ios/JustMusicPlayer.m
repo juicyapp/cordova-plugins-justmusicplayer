4641cdc83064ab17f984e9b0f7dd2eed6116c994
tszho1211 <dennis0026@gmail.com>
2016-10-13 11:41:17 +0800

    Update JustMusicPlayer.m

--- src/ios/JustMusicPlayer.m
+++ src/ios/JustMusicPlayer.m
@@ -160,11 +160,11 @@ void remoteControlReceivedWithEventImp(id self, SEL _cmd, UIEvent * event) {
             break;
         case UIEventSubtypeRemoteControlNextTrack:
             jsString = [NSString stringWithFormat:@"%@.didRemoteNextTrack();", JS_FUNCTION_NAMESPACE];
-            [[self webViewEngine] evaluateJavaScript:jsString completionHandler:nil];
+            [(UIWebView*)self.webView stringByEvaluatingJavaScriptFromString:jsString];
             break;
         case UIEventSubtypeRemoteControlPreviousTrack:
             jsString = [NSString stringWithFormat:@"%@.didRemotePreviousTrack();", JS_FUNCTION_NAMESPACE];
-            [[self webViewEngine] evaluateJavaScript:jsString completionHandler:nil];
+            [(UIWebView*)self.webView stringByEvaluatingJavaScriptFromString:jsString];
         break;
         default:
         break;
@@ -222,7 +222,7 @@ void remoteControlReceivedWithEventImp(id self, SEL _cmd, UIEvent * event) {
     AVPlayerItem *currentAudioItem = avPlayer.currentItem;
     
     NSString* jsString = [NSString stringWithFormat:@"%@.didPlayerPlaying(%f, %f);", JS_FUNCTION_NAMESPACE, CMTimeGetSeconds([currentAudioItem currentTime])*1000, CMTimeGetSeconds([currentAudioItem duration])*1000];
-    [[self webViewEngine] evaluateJavaScript:jsString completionHandler:nil];
+     [(UIWebView*)self.webView stringByEvaluatingJavaScriptFromString:jsString];
     [self updateMPInfo];
 }
 
@@ -231,7 +231,7 @@ void remoteControlReceivedWithEventImp(id self, SEL _cmd, UIEvent * event) {
     
     [self stopTimer];
     NSString* jsString = [NSString stringWithFormat:@"%@.didPlayerPlaying(%f, %f);", JS_FUNCTION_NAMESPACE, CMTimeGetSeconds([currentAudioItem currentTime])*1000, CMTimeGetSeconds([currentAudioItem duration])*1000];
-    [[self webViewEngine] evaluateJavaScript:jsString completionHandler:nil];
+     [(UIWebView*)self.webView stringByEvaluatingJavaScriptFromString:jsString];
     jsString = [NSString stringWithFormat:@"%@.didPlayerReachEnd();", JS_FUNCTION_NAMESPACE];
     [self.webView stringByEvaluatingJavaScriptFromString:jsString];
     
@@ -271,6 +271,7 @@ void remoteControlReceivedWithEventImp(id self, SEL _cmd, UIEvent * event) {
 
     // create audio
     avPlayer = [AVPlayer playerWithPlayerItem:[AVPlayerItem playerItemWithURL:[currentAlbumAudioInfo audioURL]]];
+    avPlayer.automaticallyWaitsToMinimizeStalling = NO;
     
     // add listener
     [avPlayer addObserver:self forKeyPath:@"status" options:0 context:nil];
